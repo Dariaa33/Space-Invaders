1cd7334b714f8d1f733f1379cb46367635f7625f
Dariaa33 <vesnayoon@gmail.com>
2024-12-20 10:46:31 +0100

    20.12

--- Space-Invaders/Assets/Scripts/Main Menu.cs
+++ Space-Invaders/Assets/Scripts/Main Menu.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void ExitGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.Quit();
+        Debug.Log("Quit");
     }
 }
